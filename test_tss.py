09d4429b52f7f13816ec34a74d03158d015681ec
Stefan Arentz <stefan@arentz.ca>
2017-12-11 22:45:09 -0500

    Initial import

--- test_tss.py
+++ test_tss.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 import json, time
 from pathlib import Path
@@ -8,11 +12,17 @@ import pytest
 
 import tss
 
+
+#
+# Test Fixtures
+#
+
 @pytest.fixture
 def app(tmpdir):
     app = tss.app
     app.config["STORAGE_ROOT"] = str(tmpdir)
     return app
+
 
 #
 # Utility functions
@@ -53,6 +63,7 @@ def test_make_bucket_path_create(tmpdir):
     assert path == tmpdir + "/test-bucket"
     assert path.exists() == True
 
+
 #
 # Buckets
 #
@@ -74,6 +85,7 @@ def test_delete_bucket_200(client):
     assert r.status_code == 200
     r = client.delete(url_for('delete_bucket', bucket_name="test"))
     assert r.status_code == 200
+
 
 #
 # Objects
@@ -150,171 +162,26 @@ def test_delete_object_200(client):
     assert r.status_code == 404
 
 
-# def test_delete_object(client):
+def test_mime_types(client):
 
-#     # Non existing bucket
-#     res = client.delete(url_for('delete_object', bucket_name="doesnotexist", object_name="1234567890abcdef123456.json"))
-#     assert res.status_code == 404
+    def split_content_type(content_type):
+        return content_type.split(";", maxsplit=1)[0]
 
-#     # Existing bucket, invalid object name
-#     res = client.get(url_for('delete_object', bucket_name="test", object_name="cheese.json"))
-#     assert res.status_code == 404
+    # Test if known file types come back as expected
+    for extension, mime_type in tss.KNOWN_MIME_TYPES.items():
+        object_name = "999999999999999999999999.%s" % extension
+        res = client.put(url_for('put_object', bucket_name="test_mime_types", object_name=object_name), data=mime_type)
+        assert res.status_code == 200
+        res = client.get(url_for('get_object', bucket_name="test_mime_types", object_name=object_name))
+        assert res.status_code == 200
+        assert split_content_type(res.headers['content-type']) == mime_type
 
-#     # Existing bucket, non-existing object
-#     res = client.get(url_for('delete_object', bucket_name="test", object_name="1234567890abcdef123456.json"))
-#     assert res.status_code == 404
-
-#     # Existing bucket, existing object
-#     res = client.put(url_for('put_object', bucket_name="test", object_name="4234567890abcdef12345678.txt"), data="bacon")
-#     assert res.status_code == 200
-#     res = client.delete(url_for('delete_object', bucket_name="test", object_name="4234567890abcdef12345678.txt"))
-#     assert res.status_code == 200
-#     res = client.get(url_for('delete_object', bucket_name="test", object_name="4234567890abcdef12345678.txt"))
-#     assert res.status_code == 404
-
-
-# def test_put_object(client):
-
-#     # Non-existing bucket, non-existing object
-#     res = client.put(url_for('put_object', bucket_name="test", object_name="test.txt"), data="test")
-#     assert res.status_code == 404
-
-#     # res = client.get(url_for('get_object', bucket_name="test", object_name="888888888888888888888888.txt"))
-#     # assert res.status_code == 200
-#     # assert res.data == b"broccoli"
-
-#     # Existing bucket, existing object (overwrite)
-#     res = client.put(url_for('put_object', bucket_name="test_put_object", object_name="888888888888888888888888.txt"), data="cauliflower")
-#     assert res.status_code == 200
-#     res = client.get(url_for('get_object', bucket_name="test_put_object", object_name="888888888888888888888888.txt"))
-#     assert res.status_code == 200
-#     assert res.data == b"cauliflower"
-
-
-# def test_parse_move_destination():
-
-#     bucket_name, object_id = tss.parse_location_header("/test/aaaaaaaaaaaaaaaaaaaaaaaa.txt")
-#     assert bucket_name == "test"
-#     assert object_id == "aaaaaaaaaaaaaaaaaaaaaaaa.txt"
-
-#     bucket_name, object_id = tss.parse_location_header("http://www.example.com/test/aaaaaaaaaaaaaaaaaaaaaaaa.txt")
-#     assert bucket_name is None
-#     assert object_id is None
-
-#     bucket_name, object_id = tss.parse_location_header("/")
-#     assert bucket_name is None
-#     assert object_id is None
-
-#     bucket_name, object_id = tss.parse_location_header("/test")
-#     assert bucket_name is None
-#     assert object_id is None
-
-#     bucket_name, object_id = tss.parse_location_header("/test/")
-#     assert bucket_name is None
-#     assert object_id is None
-
-#     bucket_name, object_id = tss.parse_location_header("/test/short.txt")
-#     assert bucket_name is None
-#     assert object_id is None
-
-
-# def test_parse_overwrite_header():
-
-#     assert tss.parse_overwrite_header(None) is True
-#     assert tss.parse_overwrite_header("T") is True
-#     assert tss.parse_overwrite_header("F") is False
-#     assert tss.parse_overwrite_header("Cheese") is None
-
-
-# def test_move_object(app, client):
-
-#     # Put some test files
-#     res = client.put(url_for('put_object', bucket_name="src", object_name="111111111111111111111111.txt"), data="one")
-#     assert res.status_code == 200
-#     res = client.put(url_for('put_object', bucket_name="src", object_name="222222222222222222222222.txt"), data="two")
-#     assert res.status_code == 200
-#     res = client.put(url_for('put_object', bucket_name="src", object_name="333333333333333333333333.txt"), data="three")
-#     assert res.status_code == 200
-
-#     #
-#     # Destination does not exist yet
-#     #
-
-#     # The destination does not exist
-#     res = client.get(url_for('get_object', bucket_name="dst", object_name="dddddddddddddddddddddddd.txt"))
-#     assert res.status_code == 404
-
-#     # Move it to another place
-#     res = client.open(url_for('copy_move_object', bucket_name="src", object_name="111111111111111111111111.txt"),
-#                       method="MOVE", headers={"Location": "/dst/dddddddddddddddddddddddd.txt"})
-#     assert res.status_code == 201
-
-#     # The original should be gone
-#     res = client.get(url_for('get_object', bucket_name="src", object_name="111111111111111111111111.txt"))
-#     assert res.status_code == 404
-
-#     # The destination is now there
-#     res = client.get(url_for('get_object', bucket_name="dst", object_name="dddddddddddddddddddddddd.txt"))
-#     assert res.status_code == 200
-#     assert res.data == b"one"
-
-#     #
-#     # Destination already exists
-#     #
-
-#     # Move it to another place
-#     res = client.open(url_for('copy_move_object', bucket_name="src", object_name="222222222222222222222222.txt"),
-#                       method="MOVE", headers={"Location": "/dst/dddddddddddddddddddddddd.txt"})
-#     assert res.status_code == 204
-
-#     # The original should be gone
-#     res = client.get(url_for('get_object', bucket_name="src", object_name="2222222222222222222222222.txt"))
-#     assert res.status_code == 404
-
-#     # The destination is now there
-#     res = client.get(url_for('get_object', bucket_name="dst", object_name="dddddddddddddddddddddddd.txt"))
-#     assert res.status_code == 200
-#     assert res.data == b"two"
-
-#     #
-#     # Destination already exists, Overwrite: F
-#     #
-
-#     # Move it to another place
-#     res = client.open(url_for('copy_move_object', bucket_name="src", object_name="333333333333333333333333.txt"),
-#                       method="MOVE", headers={"Location": "/dst/dddddddddddddddddddddddd.txt", "Overwrite": "F"})
-#     assert res.status_code == 412
-
-#     # The original should still be there
-#     res = client.get(url_for('get_object', bucket_name="src", object_name="333333333333333333333333.txt"))
-#     assert res.status_code == 200
-
-#     # The destination has not been touched
-#     res = client.get(url_for('get_object', bucket_name="dst", object_name="dddddddddddddddddddddddd.txt"))
-#     assert res.status_code == 200
-#     assert res.data == b"two"
-
-
-# def test_mime_types(client):
-
-#     def split_content_type(content_type):
-#         return content_type.split(";", maxsplit=1)[0]
-
-#     # Test if known file types come back as expected
-#     for extension, mime_type in tss.KNOWN_MIME_TYPES.items():
-#         object_name = "999999999999999999999999.%s" % extension
-#         res = client.put(url_for('put_object', bucket_name="test_mime_types", object_name=object_name), data=mime_type)
-#         assert res.status_code == 200
-#         res = client.get(url_for('get_object', bucket_name="test_mime_types", object_name=object_name))
-#         assert res.status_code == 200
-#         assert split_content_type(res.headers['content-type']) == mime_type
-
-#     # Test if unknown file types come back as application/octet-stream
-#     res = client.put(url_for('put_object', bucket_name="test_mime_types", object_name="aaaaaaaaaaaaaaaaaaaaaaaa.bin"), data="application/octet-stream")
-#     assert res.status_code == 200
-#     res = client.get(url_for('get_object', bucket_name="test_mime_types", object_name="aaaaaaaaaaaaaaaaaaaaaaaa.bin"))
-#     assert res.status_code == 200
-#     assert split_content_type(res.headers['content-type']) == tss.DEFAULT_MIME_TYPE
+    # Test if unknown file types come back as application/octet-stream
+    res = client.put(url_for('put_object', bucket_name="test_mime_types", object_name="aaaaaaaaaaaaaaaaaaaaaaaa.bin"), data="application/octet-stream")
+    assert res.status_code == 200
+    res = client.get(url_for('get_object', bucket_name="test_mime_types", object_name="aaaaaaaaaaaaaaaaaaaaaaaa.bin"))
+    assert res.status_code == 200
+    assert split_content_type(res.headers['content-type']) == tss.DEFAULT_MIME_TYPE
 
 
 # def test_get_buckets(app, client):
--- tss.py
+++ tss.py
@@ -60,20 +60,16 @@ def make_bucket_path(storage_root, bucket_name, create=False):
         path.mkdir(parents=True, exist_ok=True)
     return path
 
+#
+# Authentication
+#
 
-# @app.before_first_request
-# def do_something_only_once():
-#     storage_root = Path(app.config["STORAGE_ROOT"])
-#     if not storage_root.is_dir():
-#         app.logger.error("Storage root %s does not exist" % storage_root)
-
-
-# @app.before_request
-# def authenticate():
-#     api_token = app.config.get("API_TOKEN")
-#     if api_token:
-#         if request.headers.get("Authorization") != "token " + api_token:
-#             abort(401)
+@app.before_request
+def authenticate():
+    api_token = app.config.get("API_TOKEN")
+    if api_token:
+        if request.headers.get("Authorization") != "token " + api_token:
+            abort(401)
 
 #
 # Buckets
@@ -83,7 +79,6 @@ def make_bucket_path(storage_root, bucket_name, create=False):
 def put_bucket(bucket_name):
     bucket_path = make_bucket_path(app.config["STORAGE_ROOT"], bucket_name, create=True)
     return jsonify({})
-
 
 @app.route("/<bucket_name>", methods=["DELETE"])
 def delete_bucket(bucket_name):
@@ -107,7 +102,6 @@ def get_object(bucket_name, object_name):
         abort(404)
     return send_file(str(object_path), mimetype=mime_type_from_object_name(object_name))
 
-
 @app.route("/<bucket_name>/<path:object_name>", methods=["PUT"])
 def put_object(bucket_name, object_name):
     bucket_path = make_bucket_path(app.config["STORAGE_ROOT"], bucket_name, create=False)
@@ -122,7 +116,6 @@ def put_object(bucket_name, object_name):
             f.write(chunk)
     return jsonify({})
 
-
 @app.route("/<bucket_name>/<path:object_name>", methods=["DELETE"])
 def delete_object(bucket_name, object_name):
     bucket_path = make_bucket_path(app.config["STORAGE_ROOT"], bucket_name, create=False)
@@ -133,81 +126,3 @@ def delete_object(bucket_name, object_name):
         abort(404)
     object_path.unlink()
     return jsonify({})
-
-
-# def parse_location_header(location):
-#     if location and type(location) == str:
-#         m = re.match(r"/([A-Za-z0-9-_]+)/([0-9a-f]{24}\.[0-9a-z]+)",  location)
-#         if m:
-#             return m.group(1), m.group(2)
-#     return None, None
-
-
-# def parse_overwrite_header(overwrite):
-#     values = {"T": True, "F": False, None: True}
-#     return values.get(overwrite)
-
-
-# @app.route("/<bucket_name>/<objectid:object_name>", methods=["COPY", "MOVE"])
-# def copy_move_object(bucket_name, object_name):
-#     path = Path(app.config["STORAGE_ROOT"], bucket_name, hash_object_name(object_name))
-#     if not path.exists():
-#         abort(404)
-
-#     dst_bucket_name, dst_object_name = parse_location_header(request.headers.get("Location"))
-#     if dst_bucket_name is None or dst_object_name is None:
-#         abort(400, "Missing or invalid Location header")
-
-#     overwrite = parse_overwrite_header(request.headers.get("Overwrite"))
-#     if overwrite is None:
-#         abort(400, "Invalid Overwrite header")
-
-#     dst_path = Path(app.config["STORAGE_ROOT"], dst_bucket_name, hash_object_name(dst_object_name))
-#     dst_exists = dst_path.exists()
-
-#     if path == dst_path:
-#         abort(403) # Forbidden
-
-#     if not overwrite and dst_exists:
-#         abort(412) # Precondition failed
-
-#     ensure_object_directory(app.config["STORAGE_ROOT"], dst_bucket_name, dst_object_name)
-#     if request.method == "MOVE":
-#         path.rename(dst_path)
-#     else:
-#         shutil.copyfile(str(src_path), str(dst_path))
-
-#     if dst_exists:
-#         return ("OK", 204)
-#     else:
-#         return ("OK", 201)
-
-
-# @app.route("/<bucket_name>", methods=["GET"])
-# def get_bucket(bucket_name):
-#     path = Path(app.config["STORAGE_ROOT"], bucket_name)
-#     if not path.is_dir():
-#         abort(404)
-#     result = []
-#     for child in path.glob("*/*/*"):
-#         stat = child.stat()
-#         result.append({"name": child.name,
-#                        "type": mime_type_from_path(child),
-#                        "size": stat.st_size,
-#                        "modified": stat.st_mtime,
-#                        "created": stat.st_ctime})
-#     return jsonify(result)
-
-
-# @app.route("/", methods=["GET"])
-# def get_buckets():
-#     path = Path(app.config["STORAGE_ROOT"])
-#     if not path.is_dir():
-#         abort(404)
-#     result = []
-#     for child in path.glob("*"):
-#         stat = child.stat()
-#         result.append({"name": child.name,
-#                        "modified": stat.st_mtime,
-#                        "created": stat.st_ctime})
-#     return jsonify(result)
