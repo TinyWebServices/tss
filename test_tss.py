58ae93333b0414d808b96670ba2bc21c7363285b
Stefan Arentz <stefan@arentz.ca>
2017-12-11 22:55:14 -0500

    Fixed test_mime_types test

--- test_tss.py
+++ test_tss.py
@@ -167,9 +167,13 @@ def test_mime_types(client):
     def split_content_type(content_type):
         return content_type.split(";", maxsplit=1)[0]
 
+    # Create a bucket
+    r = client.put(url_for('put_bucket', bucket_name="test_mime_types"))
+    assert r.status_code == 200
+
     # Test if known file types come back as expected
     for extension, mime_type in tss.KNOWN_MIME_TYPES.items():
-        object_name = "999999999999999999999999.%s" % extension
+        object_name = "test.%s" % extension
         res = client.put(url_for('put_object', bucket_name="test_mime_types", object_name=object_name), data=mime_type)
         assert res.status_code == 200
         res = client.get(url_for('get_object', bucket_name="test_mime_types", object_name=object_name))
